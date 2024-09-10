80df3aaa013f73262e4b220e21905aaa6f5a4db4
Hossam Gouda <hos.gouda90@gmail.com>
2024-09-10 23:03:14 +0300

    update the pagination and adding support for the edit transaction path routing

--- src/App/Controllers/HomeController.php
+++ src/App/Controllers/HomeController.php
@@ -27,6 +27,30 @@ class HomeController
       $length,
       $offset
     );
-    echo $this->view->render("index.php", ['transactions' => $transactions]);
+    $lastPage = ceil($count / $length);
+    $pages = $lastPage ? range(1, $lastPage) : [];
+
+    $pageLinks = array_map(
+      fn($pageNum) => http_build_query([
+        'p' => $pageNum,
+        's' => $searchTerm
+      ]),
+      $pages
+    );
+    echo $this->view->render("index.php", [
+      'transactions' => $transactions,
+      'currentPage' => $page,
+      'previousPageQuery' => http_build_query([
+        'p' => $page - 1,
+        's' => $searchTerm
+      ]),
+      'lastPage' => $lastPage,
+      'nextPageQuery' => http_build_query([
+        'p' => $page + 1,
+        's' => $searchTerm
+      ]),
+      'pageLinks' => $pageLinks,
+      'searchTerm' => $searchTerm
+    ]);
   }
 }
--- src/Framework/Router.php
+++ src/Framework/Router.php
@@ -12,12 +12,16 @@ class Router
   public function add(string $method, string $path, array $controller)
   {
     $path = $this->normalizePath($path);
+    $regexPath = preg_replace('#{[^/]+}#', '([^/]+)', $path);
+
 
     $this->routes[] = [
       'path' => $path,
       'method' => strtoupper($method),
       'controller' => $controller,
       'middlewares' => [],
+      'regexPath' => $regexPath
+
     ];
   }
 
@@ -37,7 +41,7 @@ class Router
 
     foreach ($this->routes as $route) {
       if (
-        !preg_match("#^{$route['path']}$#", $path) ||
+        !preg_match("#^{$route['regexPath']}$#", $path) ||
         $route['method'] !== $method
       ) {
         continue;
